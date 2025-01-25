4dbd487b992a7158bc5af49cf95ae5afebc7be47
dubov <Kirill.Dubov@enterra-inc.com>
2025-01-26 02:57:57 +0300

    fix: Deploy

--- components/shared/Projects.tsx
+++ components/shared/Projects.tsx
@@ -10,7 +10,6 @@ export const Projects = () => {
 				</div>
 				<div className="flex flex-row gap-2 items-center justify-center shrink-0 relative overflow-hidden">
 					<div className="text-[#3265ed] text-left font-['Montserrat-SemiBold',_sans-serif] text-xl leading-normal font-semibold relative">Смотреть ещё</div>
-					<img className="shrink-0 w-6 h-6 relative overflow-visible" src="icon-chevron-right0.svg" />
 				</div>
 			</div>
 			<div className="flex flex-col gap-16 items-center justify-start shrink-0 relative">
@@ -18,7 +17,6 @@ export const Projects = () => {
 					<div className="flex flex-row gap-8 items-start justify-start self-stretch shrink-0 h-[380px] relative">
 						<div className="bg-color-neutral-white rounded-lg flex flex-row gap-0 items-start justify-start self-stretch shrink-0 w-[640px] relative overflow-hidden">
 							<div className="flex flex-col gap-0 items-end justify-center shrink-0 w-[640px] h-[380px] absolute left-0 top-0">
-								<img className="self-stretch shrink-0 h-[380px] relative" src="placeholder-image0.png" />
 							</div>
 							<div className="bg-[rgba(58,102,238,0.50)] p-6 flex flex-col items-start justify-between self-stretch shrink-0 w-80 relative">
 								<div className="flex flex-col gap-2 items-start justify-start self-stretch shrink-0 relative">
@@ -43,7 +41,6 @@ export const Projects = () => {
 						</div>
 						<div className="bg-color-neutral-white rounded-lg flex flex-col gap-0 items-start justify-start self-stretch flex-1 relative overflow-hidden">
 							<div className="flex flex-col gap-0 items-center justify-end shrink-0 w-[304px] h-[380px] absolute left-0 top-0">
-								<img className="self-stretch flex-1 relative" src="placeholder-image1.png" />
 							</div>
 							<div className="bg-[rgba(255,255,255,0.30)] p-6 flex flex-col gap-6 items-start justify-center self-stretch shrink-0 relative">
 								<div className="flex flex-col gap-2 items-start justify-start self-stretch shrink-0 relative">
@@ -62,21 +59,17 @@ export const Projects = () => {
 								<div className="flex flex-row gap-6 items-center justify-start self-stretch shrink-0 relative">
 									<div className="flex flex-row gap-2 items-center justify-center shrink-0 relative overflow-hidden">
 										<div className="text-[#3265ed] text-left font-['Montserrat-SemiBold',_sans-serif] text-base leading-normal font-semibold relative">Посмотреть</div>
-										<img className="shrink-0 w-6 h-6 relative overflow-visible" src="icon-chevron-right1.svg" />
 									</div>
 								</div>
-								<img className="shrink-0 w-6 h-6 absolute left-[143px] top-[272.97px] overflow-visible" src="clarity-cursor-hand-line0.svg" />
 							</div>
 						</div>
 						<div className="bg-color-neutral-white rounded-lg flex flex-col gap-0 items-start justify-start self-stretch flex-1 relative">
 							<div className="flex flex-col gap-0 items-center justify-end self-stretch flex-1 relative">
-								<img className="rounded-lg self-stretch flex-1 relative" src="placeholder-image2.png" />
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			<img className="shrink-0 w-6 h-6 absolute left-[178px] top-[526.97px] overflow-visible" src="clarity-cursor-hand-line1.svg" />
 		</div>
 	);
 };
