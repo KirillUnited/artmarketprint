ce2aa3d78b99819aaa29559bd67ce9368e9cfd56
dubov <Kirill.Dubov@enterra-inc.com>
2025-01-26 00:17:53 +0300

    feat: Update 'About' section

--- components/shared/About.tsx
+++ components/shared/About.tsx
@@ -1,47 +1,39 @@
-export const About = () => {
+import Image from 'next/image';
+import AboutImage from '../../public/images/about.jpg';
+import { Button } from '@heroui/button';
+import BrandButton from '@/components/ui/BrandButton';
+import Link from 'next/link';
+
+export default function About() {
 	return (
-		<div className="bg-background-color-primary pr-16 pl-16 flex flex-col gap-20 items-center justify-start shrink-0 relative overflow-hidden">
-			<div className="flex flex-row gap-20 items-center justify-start self-stretch shrink-0 relative">
-				<img className="shrink-0 w-[640px] h-[635px] relative" />
-				<div className="pt-[90px] pb-[90px] flex flex-col gap-[55px] items-start justify-start self-stretch flex-1 relative">
-					<div className="flex flex-col gap-4 items-start justify-start self-stretch shrink-0 relative">
-						<div className="flex flex-row gap-0 items-center justify-start shrink-0 relative">
-							<div className="text-[#2b2a28] text-left font-['Montserrat-SemiBold',_sans-serif] text-xl leading-normal font-semibold relative">О нас</div>
+		<section className="section relative overflow-hidden">
+			<div className="container">
+				<div className="py-10 md:py-20 grid md:grid-cols-2 items-center gap-x-20 gap-y-4">
+					<Image alt={'ArtMarketPrint'} className="h-full object-cover flex-1" height={635} src={AboutImage} placeholder="blur" width={640} />
+
+					<div className="flex flex-col gap-8 md:gap-16 py-10 md:py-20">
+						<div className="flex flex-col gap-4 md:gap-6">
+							<span className="text-lg font-semibold">
+								О нас
+							</span>
+							<h2 className="text-4xl md:text-6xl font-medium hyphens-auto break-words">
+								<span className="font-extrabold bg-brand-gradient text-fill-transparent">Art Market Print</span>
+								<br />
+								Печать в Минске
+							</h2>
+							<p className="text-foreground/70 text-base md:text-lg leading-normal font-light">
+								Наши услуги обеспечивают высокое качество печати и гравировки, что позволяет вам выделяться на фоне конкурентов. Мы используем современные технологии, чтобы гарантировать долговечность и яркость ваших изделий.
+							</p>
 						</div>
-						<div className="flex flex-col gap-6 items-start justify-start self-stretch shrink-0 relative">
-							<div className="text-left font-['-',_sans-serif] text-5xl leading-[120%] font-normal relative self-stretch">
-								<span>
-									<span className="heading-span">Art Market Print</span>
-									<span className="heading-span2">-</span>
-									<span className="heading-span3">Профессиональная печать</span>
-								</span>
-							</div>
-							<div className="text-[#2b2a28] text-left font-['Montserrat-Light',_sans-serif] text-lg leading-normal font-light relative self-stretch">
-								Наши услуги обеспечивают высокое качество печати и гравировки, что позволяет вам выделяться на фоне конкурентов. Мы используем современные технологии, чтобы
-								гарантировать долговечность и яркость ваших изделий.
-							</div>
-						</div>
-					</div>
-					<div className="flex flex-row gap-4 items-start justify-start shrink-0 relative">
-						<div
-							className="rounded border-solid border-[#2563eb] border pt-3 pr-6 pb-3 pl-6 flex flex-row gap-2 items-center justify-center shrink-0 w-[210px] relative overflow-hidden"
-													>
-							<div className="text-text-alternate text-left font-['Montserrat-SemiBold',_sans-serif] text-base leading-normal font-semibold relative">
-								УЗНАТЬ ЦЕНЫ
-							</div>
-						</div>
-						<div
-							className="bg-[rgba(255,255,255,0.80)] rounded border-2 border-solid pt-3 pr-6 pb-3 pl-6 flex flex-row gap-2 items-center justify-center shrink-0 w-[210px] relative overflow-hidden"
-													>
-							<div
-								className="text-left font-['Montserrat-SemiBold',_sans-serif] text-base leading-normal font-semibold relative"
-									>
-								КАТАЛОГ
-							</div>
+						<div className="flex flex-col md:flex-row gap-2 md:gap-4">
+							<BrandButton as={Link} href={'/#catalog'} state='primary'>УЗНАТЬ ЦЕНЫ</BrandButton>
+
+							<Button color='secondary' size='lg' radius='sm' variant='ghost' className='bg-brand-gradient text-fill-transparent font-semibold'>КОНСУЛЬТАЦИЯ</Button>
 						</div>
 					</div>
+
 				</div>
 			</div>
-		</div>
+		</section>
 	);
-};
+}
