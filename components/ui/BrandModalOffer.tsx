d39f3ee78843356482ff8c2614d7e3e597e6daa1
dubov <Kirill.Dubov@enterra-inc.com>
2025-01-25 23:25:34 +0300

    feat: Add native order form validation

--- components/ui/BrandModalOffer.tsx
+++ components/ui/BrandModalOffer.tsx
@@ -3,6 +3,7 @@ import { Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure
 import BrandButton from "./BrandButton";
 import { Input } from "@heroui/input";
 import { Button } from "@heroui/button";
+import { Form } from "@heroui/form";
 
 
 export default function BrandModalOffer() {
@@ -16,24 +17,32 @@ export default function BrandModalOffer() {
                     {(onClose) => (
                         <>
                             <ModalHeader className="flex flex-col gap-1">Заявка</ModalHeader>
-                            <ModalBody>
-                                <Input
-                                    label="Имя"
-                                    placeholder="Напишите Ваше имя"
-                                    variant="bordered"
-                                />
-                                <Input
-                                    label="Телефон"
-                                    placeholder="Введите Ваш телефон"
-                                    type="tel"
-                                    variant="bordered"
-                                />
-                            </ModalBody>
-                            <ModalFooter>
-                                <Button color='secondary' size='lg' radius='sm' variant='ghost' className='bg-brand-gradient text-fill-transparent font-semibold' onPress={onClose}>ОТМЕНА</Button>
 
-                                <BrandButton state='primary' className='flex-1 basis-32'>ЗАКАЗАТЬ</BrandButton>
-                            </ModalFooter>
+                            <Form validationBehavior="native">
+                                <ModalBody className="w-full">
+                                    <Input
+                                        isRequired
+                                        errorMessage="Пожалуйста, введите Ваше имя"
+                                        label="Имя"
+                                        placeholder="Напишите Ваше имя"
+                                        variant="bordered"
+                                    />
+                                    <Input
+                                        isRequired
+                                        errorMessage="Пожалуйста, введите действительный номер телефона"
+                                        label="Телефон"
+                                        placeholder="Введите Ваш телефон"
+                                        type="tel"
+                                        variant="bordered"
+                                    />
+                                </ModalBody>
+                                <ModalFooter className="w-full">
+                                    <Button color='secondary' size='lg' radius='sm' variant='ghost' className='bg-brand-gradient text-fill-transparent font-semibold' onPress={onClose}>ОТМЕНА</Button>
+
+                                    <BrandButton type="submit" state='primary' className='flex-1 basis-32'>ЗАКАЗАТЬ</BrandButton>
+                                </ModalFooter>
+
+                            </Form>
                         </>
                     )}
                 </ModalContent>
