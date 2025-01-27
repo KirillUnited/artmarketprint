713974d92cb963b232e2ea7f6ca09822eeb14784
dubov <Kirill.Dubov@enterra-inc.com>
2025-01-27 19:58:21 +0300

    feat: Adjust 'Services' section

--- components/ui/BrandModalOffer.tsx
+++ components/ui/BrandModalOffer.tsx
@@ -13,7 +13,7 @@ export default function BrandModalOffer() {
     return (
         <>
             <BrandButton className='flex-1 basis-52' state='primary' onPress={onOpen}>ЗАКАЗАТЬ</BrandButton>
-            <Modal backdrop="blur" isOpen={isOpen} placement="top-center" onOpenChange={onOpenChange} className='bg-[#F1F4FA]'>
+            <Modal backdrop="blur" isOpen={isOpen} placement="top-center" onOpenChange={onOpenChange} className='bg-background'>
                 <ModalContent>
                     {(onClose) => (
                         <>
