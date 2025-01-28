6f8cea14a45a24eb6a4e258dd17b38f30688e23a
dubov <Kirill.Dubov@enterra-inc.com>
2025-01-28 23:20:41 +0300

    feat: Add telegram live chat

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -6,6 +6,7 @@ import { Catalog } from '@/components/shared/Catalog';
 import SocialWidget from '@/components/shared/SocialWidget';
 import { Services } from '@/components/shared/Services';
 import Contacts from '@/components/shared/Contacts';
+import Script from 'next/script';
 
 export default function Home() {
 	return (
@@ -20,6 +21,12 @@ export default function Home() {
 			<Contacts />
 			<SocialWidget />
 			{/* <Testimonials /> */}
+			<Script
+				src="https://static.elfsight.com/platform/platform.js"
+				strategy="lazyOnload"
+				async
+			/>
+			<div className="elfsight-app-7f56de0f-7490-4845-801c-cf32d1a5aeb1" data-elfsight-app-lazy></div>
 		</>
 	);
 }
