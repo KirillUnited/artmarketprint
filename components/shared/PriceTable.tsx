0da5910a34b38d97201c3c2e8debaeb018dbfb94
dubov <Kirill.Dubov@enterra-inc.com>
2025-01-29 19:52:07 +0300

    fix: PriceTable build issue

--- components/shared/PriceTable.tsx
+++ components/shared/PriceTable.tsx
@@ -1,66 +1,54 @@
 'use client';
 import React from 'react';
 import {Table, TableHeader, TableBody, TableColumn, TableRow, TableCell, getKeyValue} from '@heroui/table';
-import {useAsyncList} from '@react-stately/data';
 import {Spinner} from "@heroui/spinner";
 
+const list = {
+	items: [
+		{
+			name: 'Luke Skywalker',
+			height: '172',
+			mass: '77',
+			birth_year: '19BBY',
+		},
+		{
+			name: 'C-3PO',
+			height: '167',
+			mass: '75',
+			birth_year: '112BBY',
+		},
+		{
+			name: 'R2-D2',
+			height: '96',
+			mass: '32',
+			birth_year: '33BBY',
+		},
+	],
+}
+
 export default function PriceTable() {
-	const [isLoading, setIsLoading] = React.useState(true);
-
-	let list = useAsyncList({
-		async load({signal}) {
-			let res = await fetch('https://swapi.py4e.com/api/people/?search', {
-				signal,
-			});
-			let json = await res.json();
-
-			setIsLoading(false);
-
-			return {
-				items: json.results,
-			};
-		},
-		async sort({items, sortDescriptor}) {
-			return {
-				items: items.sort((a, b) => {
-					let first = a[sortDescriptor.column];
-					let second = b[sortDescriptor.column];
-					let cmp = (parseInt(first) || first) < (parseInt(second) || second) ? -1 : 1;
-
-					if (sortDescriptor.direction === 'descending') {
-						cmp *= -1;
-					}
-
-					return cmp;
-				}),
-			};
-		},
-	});
-
 	return (
 		<Table
 			aria-label="Example table with client side sorting"
 			classNames={{
-				table: 'min-h-[400px]',
+				table: '',
 			}}
-			sortDescriptor={list.sortDescriptor}
-			onSortChange={list.sort}
 		>
 			<TableHeader>
-				<TableColumn key="name" allowsSorting>
+				<TableColumn key="name">
 					Name
 				</TableColumn>
-				<TableColumn key="height" allowsSorting>
+				<TableColumn key="height">
 					Height
 				</TableColumn>
-				<TableColumn key="mass" allowsSorting>
+				<TableColumn key="mass">
 					Mass
 				</TableColumn>
-				<TableColumn key="birth_year" allowsSorting>
+				<TableColumn key="birth_year">
 					Birth year
 				</TableColumn>
 			</TableHeader>
-			<TableBody isLoading={isLoading} items={list.items} loadingContent={<Spinner label="Loading..." />}>
+			<TableBody items={list.items} loadingContent={<Spinner label="Loading..." />}>
 				{(item) => <TableRow key={item.name}>{(columnKey) => <TableCell>{getKeyValue(item, columnKey)}</TableCell>}</TableRow>}
 			</TableBody>
 		</Table>
