e14ffe80245c92f25b3bfb2f94e7bf95fa2574ca
dubov <Kirill.Dubov@enterra-inc.com>
2025-02-13 21:33:33 +0300

    feat(Header): create Navbar.tsx

--- components/shared/Navbar.tsx
+++ components/shared/Navbar.tsx
@@ -1,38 +1,152 @@
-import Image from 'next/image';
+'use client';
+import React from 'react';
+import { Button } from '@heroui/button';
+import { Navbar as BaseNavbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenu, NavbarMenuItem, NavbarMenuToggle } from '@heroui/navbar';
+import { Link } from '@heroui/link';
+import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from '@heroui/dropdown';
+import { ChevronDownIcon } from 'lucide-react';
+
+import { PhoneIcon, SearchIcon, TelegramIcon, ViberIcon } from '../icons';
+import BrandLogo from '../ui/BrandLogo';
+import { HeroModalOffer } from '../ui/BrandModalOffer';
+
+import Socials from './Socials';
+
+import { siteConfig } from '@/config/site';
+import {PhoneListDropdown} from '@/components/ui/PhoneListDropdown';
+
+type HeaderDropdownMenuProps = {
+	triggerLabel: string,
+	items: { label: string, href?: string, description?: string }[]
+}
 
 export default function Navbar() {
+	const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
 	return (
-		<div className="bg-background-color-primary border-solid border-[#f1f4fa] border pr-16 pl-16 flex flex-col gap-0 items-center justify-center shrink-0 h-[72px] relative">
-			<div className="flex flex-row items-center justify-between self-stretch shrink-0 relative">
-				<div className="shrink-0 w-[184px] h-10 static">
-					<div className="text-left font-['Montserrat-Black',_sans-serif] text-base leading-normal font-black absolute left-[51px] top-2">ArtMarketPrint</div>
-					<Image alt={'ArtMarketPrint'} className="flex flex-row gap-0 items-center justify-center w-[35px] h-10 absolute left-0 top-0" height={40} src="/images/logo.png"  width={40}/>
-				</div>
-				<div className="flex flex-row gap-8 items-center justify-end shrink-0 relative">
-					<div className="flex flex-row gap-1 items-center justify-center shrink-0 relative">
-						<div className="text-[#2b2a28] text-left font-['Montserrat-SemiBold',_sans-serif] text-base leading-normal font-semibold relative">Услуги</div>
+		<BaseNavbar
+			shouldHideOnScroll
+			classNames={{
+				base: 'shadow-medium',
+				wrapper: 'max-w-full p-0 items-center',
+			}}
+			isMenuOpen={isMenuOpen}
+			onMenuOpenChange={setIsMenuOpen}
+			onScrollPositionChange={() => setIsMenuOpen(false)}
+		>
+			<div className="container flex flex-row items-center justify-between gap-4">
+				<NavbarBrand as={Link} className="grow-0 basis-auto" href={'/'}>
+					<BrandLogo alt={'ArtMarketPrint'} />
+				</NavbarBrand>
+				<NavbarContent className="hidden xl:flex gap-8" justify="center">
+					{siteConfig?.navItems?.map((navItem, index) => {
+						return (
+							navItem?.menuItems ?
+								(
+									<NavbarDropdownMenu key={navItem.label} items={navItem.menuItems} triggerLabel={navItem.label} />
+								) : (
+									<NavbarItem key={navItem.label} isActive={index === 0}>
+										<Link aria-current="page" className="leading-normal font-semibold hover:underline hover:text-primary transition" color={'foreground'} href={navItem.href} size="sm">
+											{navItem.label}
+										</Link>
+									</NavbarItem>
+								)
+						)
+					})}
+				</NavbarContent>
+				<div className="flex flex-row gap-8 items-center shrink-0">
+					<SearchIcon />
+					<div className="hidden md:flex gap-2">
+						<Link
+							className="flex flex-row gap-4 items-center justify-start self-stretch shrink-0 relative"
+							href={`https://t.me/${siteConfig?.contacts?.[0]?.list?.[0]?.href}`}
+							target="_blank"
+						>
+							<TelegramIcon />
+						</Link>
+						<Link href={`https://msng.link/o?${siteConfig?.contacts?.[0].list?.[0]?.href}=vi`} target="_blank">
+							<ViberIcon />
+						</Link>
 					</div>
-					<div className="flex flex-row gap-1 items-center justify-center shrink-0 relative">
-						<div className="text-[#2b2a28] text-left font-['Montserrat-SemiBold',_sans-serif] text-base leading-normal font-semibold relative">Каталог/Цены</div>
-					</div>
-					<div className="bg-[#ffffff] flex flex-row gap-1 items-center justify-center shrink-0 relative">
-						<div className="text-[#3265ed] text-left font-['Montserrat-Bold',_sans-serif] text-base leading-normal font-bold relative">Проекты</div>
-					</div>
-					<div className="flex flex-row gap-1 items-center justify-center shrink-0 relative">
-						<div className="text-[#2b2a28] text-left font-['Montserrat-SemiBold',_sans-serif] text-base leading-normal font-semibold relative">Отзывы</div>
-					</div>
-					<div className="flex flex-row gap-1 items-center justify-center shrink-0 relative">
-						<div className="text-[#2b2a28] text-left font-['Montserrat-SemiBold',_sans-serif] text-base leading-normal font-semibold relative">Контакты</div>
-					</div>
-				</div>
-				<div className="flex flex-row gap-8 items-center justify-center shrink-0 relative">
-					<div className="flex flex-row gap-4 items-center justify-center shrink-0 relative">
-						<div className="bg-[#2563eb] rounded-lg pt-1 pr-4 pb-1 pl-4 flex flex-row gap-2 items-center justify-start shrink-0 relative">
-							<div className="text-text-alternate text-left font-['Roboto-SemiBold',_sans-serif] text-base leading-normal font-semibold relative">+375 (29) 999-99-99</div>
-						</div>
-					</div>
+
+					<PhoneListDropdown />
+					{/*
+					<Button className="leading-normal font-semibold hidden lg:flex" color="primary" variant="solid" radius='sm'>
+						<CalendarIcon size={18} />
+						<span>ЗАКАЗАТЬ ЗВОНОК</span>
+					</Button> */}
+					<div className='hidden lg:block'><HeroModalOffer/></div>
+					<NavbarMenuToggle aria-label={isMenuOpen ? 'Close menu' : 'Open menu'} className="xl:hidden h-6" />
 				</div>
 			</div>
-		</div>
+			<NavbarMenu className="gap-6 px-4 py-6">
+				<div className="flex flex-col gap-4">
+					{siteConfig?.navItems.map((navItem, index) => (
+						<NavbarMenuItem key={`${navItem}-${index}`}>
+							<Link className="w-full" color="foreground" href={navItem.href} size="lg" >
+								{navItem.label}
+							</Link>
+						</NavbarMenuItem>
+					))}
+				</div>
+				{/*<Button className="leading-normal font-semibold" color="primary" variant="solid" radius='sm'>*/}
+				{/*	<span>ЗАКАЗАТЬ ЗВОНОК</span>*/}
+				{/*</Button>*/}
+				<HeroModalOffer />
+				<div className="flex flex-col gap-4">
+					{siteConfig?.contacts?.[0]?.list?.map((item) => (
+						<Link key={item.href} className="font-bold text-left flex items-center gap-2" href={`tel:${item.href}` || '#'}>
+							<PhoneIcon size={20} />
+							{item.label}
+						</Link>
+					))}
+				</div>
+				<Socials />
+			</NavbarMenu>
+		</BaseNavbar>
 	);
 };
+
+const NavbarDropdownMenu = ({ triggerLabel, items }: HeaderDropdownMenuProps) => {
+	return (
+		<Dropdown
+			classNames={{
+				content: 'rounded-md',
+			}}>
+			<NavbarItem>
+				<DropdownTrigger>
+					<Button
+						disableRipple
+						className="p-0 bg-transparent data-[hover=true]:bg-transparent leading-normal font-semibold hover:underline hover:text-primary transition gap-1"
+						endContent={<ChevronDownIcon className="text-primary" size={20} />}
+						radius="sm"
+						size="md"
+						variant="light"
+					>
+						{triggerLabel}
+					</Button>
+				</DropdownTrigger>
+			</NavbarItem>
+			<DropdownMenu
+				aria-label="Услуги"
+				className="w-[340px]"
+				itemClasses={{
+					base: 'gap-4 rounded-md data-[hover=true]:bg-brand-gradient data-[hover=true]:text-fill-transparent',
+					title: 'font-semibold',
+				}}
+			>
+				{
+					items.map((item) => (
+						<DropdownItem
+							key={item.label}
+							description={item.description}
+							href={item.href}
+						>
+							{item.label}
+						</DropdownItem>
+					))
+				}
+			</DropdownMenu>
+		</Dropdown>
+	)
+}
