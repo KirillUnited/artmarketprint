b1c51c14dbc0904f3018ab739068c4553471883b
dubov <Kirill.Dubov@enterra-inc.com>
2025-02-20 01:58:40 +0300

    feat: display hero content

--- components/shared/hero/HeroContent.tsx
+++ components/shared/hero/HeroContent.tsx
@@ -12,20 +12,9 @@ import { PortableText } from 'next-sanity';
 export default function HeroContent({ title, description, subtitle }: { title?: any, description?: string, subtitle?: string }) {
     return (
         <>
-            <motion.div
-                className="bg-[rgba(255,255,255,0.50)] backdrop-blur-md rounded-lg py-10 md:py-16 pr-4 md:pr-7 flex flex-col gap-8 md:gap-16 z-10 max-w-full md:max-w-4xl overflow-hidden"
-                initial={{
-                    opacity: 0,
-                    translate: '-100%',
-                }}
-                transition={{ duration: 1, ease: 'easeInOut' }}
-                viewport={{ once: true, amount: 0 }}
-                whileInView={{
-                    opacity: 1,
-                    translate: '0',
-                }}
-            >
-                <div className="flex flex-col gap-4 md:gap-6">
+            <div className="flex flex-col gap-4 md:gap-6">
+                {
+                    subtitle &&
                     <motion.span
                         className="text-primary uppercase text-base leading-normal font-bold"
                         initial={{
@@ -41,45 +30,48 @@ export default function HeroContent({ title, description, subtitle }: { title?: 
                     >
                         - {subtitle} -
                     </motion.span>
-                    <motion.h1
-                        className="text-4xl md:text-5xl lg:text-6xl leading-none font-medium break-words text-balance"
-                        initial={{
-                            opacity: 0,
-                            translate: '100% 0',
-                        }}
-                        transition={{ duration: 2.5, ease: 'easeInOut' }}
-                        viewport={{ once: true, amount: 0 }}
-                        whileInView={{
-                            opacity: 1,
-                            translate: '0',
-                        }}
-                    >
-                        <span className="hidden md:inline">Профессиональная</span> <span className="hidden md:inline">печать</span> <span className="md:hidden">Печать</span> на
-                        <span className="font-extrabold"> любых материалах </span>в<span className="font-extrabold bg-brand-gradient text-fill-transparent"> Минске</span>
-                        {/* <PortableText value={title} /> */}
-                    </motion.h1>
+                }
+                <motion.div
+                    className="text-4xl md:text-5xl lg:text-6xl leading-none font-medium break-words text-balance"
+                    initial={{
+                        opacity: 0,
+                        translate: '100% 0',
+                    }}
+                    transition={{ duration: 2.5, ease: 'easeInOut' }}
+                    viewport={{ once: true, amount: 0 }}
+                    whileInView={{
+                        opacity: 1,
+                        translate: '0',
+                    }}
+                >
+                    {/* <span className="hidden md:inline">Профессиональная</span> <span className="hidden md:inline">печать</span> <span className="md:hidden">Печать</span> на
+                        <span className="font-extrabold"> любых материалах </span>в<span className="font-extrabold bg-brand-gradient text-fill-transparent"> Минске</span> */}
+                    <PortableText value={title} />
+                </motion.div>
+                {
+                    description &&
                     <p className="text-foreground/70 text-base md:text-lg leading-normal font-medium">{description}</p>
-                </div>
-                <div className="flex flex-col md:flex-row gap-2 md:gap-4">
-                    <BrandButton as={Link} href={'/#services'} state="primary">
-                        УЗНАТЬ ЦЕНЫ
-                    </BrandButton>
+                }
+            </div>
+            <div className="flex flex-col md:flex-row gap-2 md:gap-4">
+                <BrandButton as={Link} href={'/#services'} state="primary">
+                    УЗНАТЬ ЦЕНЫ
+                </BrandButton>
 
-                    <Button
-                        as={Link}
-                        className="bg-brand-gradient text-fill-transparent font-semibold"
-                        color="secondary"
-                        href={'/catalog'}
-                        radius="sm"
-                        size="lg"
-                        target="_blank"
-                        variant="bordered"
-                    >
-                        <span className="leading-none">КАТАЛОГ</span>
-                        <ArrowUpRightIcon className="text-secondary" size={18} />
-                    </Button>
-                </div>
-            </motion.div>
+                <Button
+                    as={Link}
+                    className="bg-brand-gradient text-fill-transparent font-semibold"
+                    color="secondary"
+                    href={'/catalog'}
+                    radius="sm"
+                    size="lg"
+                    target="_blank"
+                    variant="bordered"
+                >
+                    <span className="leading-none">КАТАЛОГ</span>
+                    <ArrowUpRightIcon className="text-secondary" size={18} />
+                </Button>
+            </div>
         </>
     )
 }
