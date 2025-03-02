642c3482d664c182b26e6ab5d6e62d3333fcd41a
dubov <Kirill.Dubov@enterra-inc.com>
2025-03-02 14:21:43 +0300

    feat(Projects): create project gallery component

--- components/shared/project/ProjectGallery.tsx
+++ components/shared/project/ProjectGallery.tsx
@@ -1,31 +1,29 @@
 'use client';
 import React from 'react';
-import { SlideshowLightbox, initLightboxJS } from 'lightbox.js-react'
-import Image from 'next/image';
+import { SlideshowLightbox } from 'lightbox.js-react'
 
-export default function ProjectGallery({ items }: any) {
-    console.log('items', items);
+/**
+ * @typedef {Object} ProjectGalleryItem
+ * @property {_key: string} _key - A unique key for the image
+ * @property {string} imageUrl - The URL of the image
+ * @property {string} altText - The alt text for the image
+ */
 
-    React.useEffect(() => {
-        initLightboxJS("Insert License key", "Insert plan type here");
-    }, []);
+/**
+ * A React component that renders a grid of images which can be clicked to open a lightbox.
+ * @param {ProjectGalleryItem[]} items - The list of items to render
+ * @returns {React.ReactElement} The rendered component
+ */
 
+export type ProjectGalleryItem = { _key: string, imageUrl: string, altText: string };
+export interface ProjectGalleryProps {
+    items: ProjectGalleryItem[]
+}
+
+export default function ProjectGallery({ items }: ProjectGalleryProps): React.ReactElement {
     return (
-        // <SlideshowLightbox className='grid grid-cols-1 md:grid-cols-3 gap-4' showThumbnails={true}>
-        //     {items.map(({ _key, imageUrl, altText }: { _key: string, imageUrl: string, altText: string }) => <Image key={_key} className='w-full rounded' src={imageUrl} alt={altText} width={320} height={320} />)}
-        // </SlideshowLightbox>
-        <SlideshowLightbox lightboxIdentifier="lightbox1" framework="next" images={items}>
-            {items.map(({ _key, imageUrl, altText }: { _key: string, imageUrl: string, altText: string }) => (
-                <Image
-                    key={_key}
-                    src={imageUrl}
-                    alt={altText}
-                    height={500}
-                    width={500}
-                    data-lightboxjs="lightbox1"
-                    quality={80}
-                />
-            ))}
+        <SlideshowLightbox className='grid grid-cols-1 md:grid-cols-3 gap-4' showThumbnails={true}>
+            {items.map(({ _key, imageUrl, altText }: ProjectGalleryItem) => <img key={_key} className='w-full rounded-small aspect-square object-cover' src={imageUrl} alt={altText} width={320} height={320} data-lightboxjs="lightbox1" loading='lazy' decoding='async' />)}
         </SlideshowLightbox>
     )
 }
