2dd7fc1bf9a79679660f975f43943293b2a028a0
dubov <Kirill.Dubov@enterra-inc.com>
2025-03-05 09:31:14 +0300

    feat(ProductStore): update filter UI

--- components/shared/product/ProductsFilter.tsx
+++ components/shared/product/ProductsFilter.tsx
@@ -5,23 +5,28 @@ import React from 'react'
 
 export default function ProductsFilter(props: any) {
   return (
-    <div className='flex flex-wrap gap-4'>
-      <Select label="Фильтр по категориям" labelPlacement='outside' radius='sm' size='sm'>
-        {props?.categories?.map((category: string) => (
-          <SelectItem key={category}>
-            {category}
+    <div className='flex gap-4 justify-between sticky top-0 z-10 bg-background py-4 border-bottom border-y
+    '>
+      <div className='flex gap-4 flex-1'>
+        <Select label="Фильтр по категориям" labelPlacement='outside' radius='sm' size='sm' selectionMode='multiple' className='w-60'>
+          {props?.categories?.map((category: string) => (
+            <SelectItem key={category}>
+              {category}
+            </SelectItem>
+          ))}
+        </Select>
+        <Input label='Поиск товара' labelPlacement='outside' variant='bordered' type='search' placeholder='Поиск' radius='sm' size='sm' />
+      </div>
+      <div className='flex gap-4'>
+        <Select label="Сортировать" labelPlacement='outside' radius='sm' size='sm' className='w-60'>
+          <SelectItem key="1" textValue='По убыванию'>
+            По убыванию
           </SelectItem>
-        ))}
-      </Select>
-      <Input label='Поиск товара' labelPlacement='outside' variant='bordered' type='search' placeholder='Поиск' radius='sm' size='sm' className='flex-1' />
-      <Select label="Сортировать" labelPlacement='outside' radius='sm' size='sm'>
-        <SelectItem key="1" textValue='По убыванию'>
-          По убыванию
-        </SelectItem>
-        <SelectItem key="2" textValue='По возрастанию'>
-          По возрастанию
-        </SelectItem>
-      </Select>
+          <SelectItem key="2" textValue='По возрастанию'>
+            По возрастанию
+          </SelectItem>
+        </Select>
+      </div>
     </div>
   )
 }
