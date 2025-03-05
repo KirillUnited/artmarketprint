bf68a17a6b720960394a8f3441f0f9802f054f38
dubov <Kirill.Dubov@enterra-inc.com>
2025-03-05 09:07:18 +0300

    feat(ProductStore): create price filter

--- components/shared/product/ProductsFilter.tsx
+++ components/shared/product/ProductsFilter.tsx
@@ -6,14 +6,22 @@ import React from 'react'
 export default function ProductsFilter(props: any) {
   return (
     <div className='flex flex-wrap gap-4'>
-      <Select label="Фильтр по категориям" radius='sm' size='sm' className='max-w-xs'>
+      <Select label="Фильтр по категориям" labelPlacement='outside' radius='sm' size='sm'>
         {props?.categories?.map((category: string) => (
           <SelectItem key={category}>
             {category}
           </SelectItem>
         ))}
       </Select>
-      <Input label='Поиск товара' variant='bordered' type='search' placeholder='Поиск' radius='sm' size='sm' className='flex-1' />
+      <Input label='Поиск товара' labelPlacement='outside' variant='bordered' type='search' placeholder='Поиск' radius='sm' size='sm' className='flex-1' />
+      <Select label="Сортировать" labelPlacement='outside' radius='sm' size='sm'>
+        <SelectItem key="1" textValue='По убыванию'>
+          По убыванию
+        </SelectItem>
+        <SelectItem key="2" textValue='По возрастанию'>
+          По возрастанию
+        </SelectItem>
+      </Select>
     </div>
   )
 }
