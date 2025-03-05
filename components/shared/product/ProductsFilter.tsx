b24f815f9fe4dbf451d1334711492676b3e20766
dubov <Kirill.Dubov@enterra-inc.com>
2025-03-05 09:48:54 +0300

    feat(ProductStore): ui updates

--- components/shared/product/ProductsFilter.tsx
+++ components/shared/product/ProductsFilter.tsx
@@ -8,7 +8,7 @@ export default function ProductsFilter(props: any) {
     <div className='flex gap-4 justify-between sticky top-0 z-10 bg-background py-4 border-bottom border-y
     '>
       <div className='flex gap-4 flex-1'>
-        <Select label="Фильтр по категориям" labelPlacement='outside' radius='sm' size='sm' selectionMode='multiple' className='w-60'>
+        <Select label="Фильтр по категориям" labelPlacement='outside' aria-label='Select category' radius='sm' size='sm' selectionMode='multiple' className='hidden md:block w-60'>
           {props?.categories?.map((category: string) => (
             <SelectItem key={category}>
               {category}
@@ -18,7 +18,7 @@ export default function ProductsFilter(props: any) {
         <Input label='Поиск товара' labelPlacement='outside' variant='bordered' type='search' placeholder='Поиск' radius='sm' size='sm' />
       </div>
       <div className='flex gap-4'>
-        <Select label="Сортировать" labelPlacement='outside' radius='sm' size='sm' className='w-60'>
+        <Select label="Сортировать" labelPlacement='outside' radius='sm' size='sm' className='hidden md:block w-60'>
           <SelectItem key="1" textValue='По убыванию'>
             По убыванию
           </SelectItem>
