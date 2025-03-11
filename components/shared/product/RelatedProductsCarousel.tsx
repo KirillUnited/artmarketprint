735050bfb05a567399a74220eea9f4d7a0913221
dubov <Kirill.Dubov@enterra-inc.com>
2025-03-11 19:26:58 +0300

    refactor(ProductStore): create product thumbnail

--- components/shared/product/RelatedProductsCarousel.tsx
+++ components/shared/product/RelatedProductsCarousel.tsx
@@ -1,18 +1,14 @@
 'use client';
 import React, { useEffect } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Card, CardBody, CardFooter } from '@heroui/card';
-import { Button } from '@heroui/button';
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import { Navigation, Pagination } from 'swiper/modules';
-import { Image } from '@heroui/image';
-import { Link } from '@heroui/link';
 import Loader from '@/components/ui/Loader';
-import { getPrice } from '@/lib/getPrice';
 import styles from './product.module.css';
+import ProductThumb from './ProductThumb';
 
 export default function RelatedProductsCarousel({ relatedProducts }: any) {
     const [products, setProducts] = React.useState([]);
@@ -53,17 +49,7 @@ export default function RelatedProductsCarousel({ relatedProducts }: any) {
         >
             {products?.map((item: any) => (
                 <SwiperSlide key={`${item?.id["#text"]}`} className='lg:min-w-64'>
-                    <Card className="h-full group relative max-w-full shadow-sm" radius="sm" >
-                        <CardBody as={Link} href={`/products/${item?.id["#text"]}`} className='items-stretch'>
-                            <Image removeWrapper alt={item.altText} className="object-cover aspect-square mx-auto" radius="sm" src={item.images_urls?.split(",")[0]} width={220} />
-                            <span className="text-xl md:text-2xl text-primary font-semibold self-start">{`${getPrice(item.price, 1.1)} BYN`}</span>
-                            <h3 className="text-2xl font-bold text-gray-900 line-clamp-2">{item.product?.__cdata}</h3>
-                            <p className="text-gray-600 line-clamp-2">{item.general_description?.__cdata}</p>
-                        </CardBody>
-                        <CardFooter>
-                            <Button as={Link} target='_blank' href={`/products/${item?.id["#text"]}`} size="md" color='secondary' radius='sm'>Подробнее</Button>
-                        </CardFooter>
-                    </Card>
+                    <ProductThumb item={item} />
                 </SwiperSlide>
             ))}
         </Swiper>
