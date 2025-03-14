07f858e802281fef190e7f3c437c4f12a89406ef
dubov <Kirill.Dubov@enterra-inc.com>
2025-03-14 18:37:19 +0300

    refactor(Navbar): comment header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -6,16 +6,25 @@ import { client } from '@/sanity/client';
 import { NAVIGATION_QUERY } from '@/sanity/lib/queries';
 import { getSanityDocuments } from '@/lib/fetch-sanity-data';
 
+/**
+ * Header component that fetches navigation and sales data from Sanity and passes it to the Navbar component.
+ * @returns {JSX.Element} The Navbar component with navigation and sales data.
+ */
 export default async function Header() {
-	const navigation = await client.fetch(NAVIGATION_QUERY);
-	const sales: any = await getSanityDocuments(defineQuery(`*[_type == "salesType" && isActive][0] {
-		_id,
-		title,
-		description,
-		isActive,
-		discountPercentage,
-		products[0]->
-		}`)) || null;
+    // Fetch navigation data from Sanity
+    const navigation = await client.fetch(NAVIGATION_QUERY);
 
-	return <Navbar navigation={navigation[0].links} products={sales?.products} sales={sales} />;
+    // Fetch active sales data from Sanity
+    const salesQuery = defineQuery(`*[_type == "salesType" && isActive][0] {
+        _id,
+        title,
+        description,
+        isActive,
+        discountPercentage,
+        products[0]->
+    }`);
+    const sales: any = await getSanityDocuments(salesQuery) || null;
+
+    // Return the Navbar component with fetched data
+    return <Navbar navigation={navigation[0].links} products={sales?.products} sales={sales} />;
 }
