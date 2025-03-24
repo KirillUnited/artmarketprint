cd0f4aa7627e22ec5164478dc77d54f6cb8dd1bd
dubov <Kirill.Dubov@enterra-inc.com>
2025-03-24 13:07:31 +0300

    refactor(ProductsFilter): replace local filter components with imports from UI library for better modularity

---
+++ components/ui/filter/CatFilter.tsx
@@ -0,0 +1,28 @@
+import { Radio, RadioGroup } from "@heroui/radio";
+import { FC } from "react";
+
+interface CatFilterProps {
+    sortOrder: string;
+    onFilterChange: (sortOrder: string, category: string) => void;
+    categories: string[];
+}
+
+/**
+ * Component for filtering products by category.
+ *
+ * @param {CatFilterProps} props - The properties for the CatFilter component.
+ * @returns {JSX.Element} The rendered CatFilter component.
+ */
+export const CatFilter: FC<CatFilterProps> = ({ sortOrder, onFilterChange, categories }) => {
+    return (
+        <div className='flex gap-4 flex-1'>
+            <RadioGroup classNames={{ label: 'font-semibold text-foreground' }} label='Фильтр по категориям' title='Фильтр по категориям'>
+                {categories?.map((category: string) => (
+                    <Radio key={category} value={category} onChange={(e) => onFilterChange(sortOrder, e.target.value)}>
+                        {category}
+                    </Radio>
+                ))}
+            </RadioGroup>
+        </div>
+    )
+};
