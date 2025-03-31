93e680d6f2d37decb2bb9b43e71e3d42c55b6d9b
dubov <Kirill.Dubov@enterra-inc.com>
2025-03-31 12:38:56 +0300

    refactor: increase product limit and remove unused code
    Increase the product limit from 3000 to 5000 in the products page to accommodate more products. Remove unused imports and commented-out code in the home page to improve code cleanliness and maintainability.

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -1,13 +1,9 @@
-import Script from 'next/script';
-
 import { getSanityDocuments } from '@/lib/fetch-sanity-data';
 import { HOME_PAGE_QUERY } from '@/sanity/lib/page.query';
 import { PageBuilder } from '@/components/PageBuilder';
-import { getXmlData } from '@/lib/fetch-artegifts-data';
 
 export default async function Home() {
 	const data: any = await getSanityDocuments(HOME_PAGE_QUERY);
-	const products = await getXmlData();
 
 	if (!Array.isArray(data?.homePage?.content) || data?.homePage.length === 0) {
 		console.warn('Нет данных о главной странице');
@@ -18,12 +14,6 @@ export default async function Home() {
 	return (
 		<>
 			<PageBuilder content={data?.homePage?.content} />
-			{/* <Script
-			async
-			src="https://static.elfsight.com/platform/platform.js"
-			strategy="lazyOnload"
-			/>
-			<div data-elfsight-app-lazy className="elfsight-app-7f56de0f-7490-4845-801c-cf32d1a5aeb1" /> */}
 		</>
 	);
 }
