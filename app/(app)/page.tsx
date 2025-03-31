f3359e0692b34041b52c315b2604d5cbbbe34e10
dubov <Kirill.Dubov@enterra-inc.com>
2025-03-31 12:31:14 +0300

    refactor(api): remove deprecated products route
    The products route was deleted as it is no longer needed in the current API structure. This change simplifies the codebase by removing unused code.

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -3,9 +3,11 @@ import Script from 'next/script';
 import { getSanityDocuments } from '@/lib/fetch-sanity-data';
 import { HOME_PAGE_QUERY } from '@/sanity/lib/page.query';
 import { PageBuilder } from '@/components/PageBuilder';
+import { getXmlData } from '@/lib/fetch-artegifts-data';
 
 export default async function Home() {
 	const data: any = await getSanityDocuments(HOME_PAGE_QUERY);
+	const products = await getXmlData();
 
 	if (!Array.isArray(data?.homePage?.content) || data?.homePage.length === 0) {
 		console.warn('Нет данных о главной странице');
