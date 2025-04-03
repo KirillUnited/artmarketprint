679b4310ad8d6299607c6256bc82d67551356099
dubov <Kirill.Dubov@enterra-inc.com>
2025-04-03 14:47:59 +0300

    chore: update Sanity client project ID
    The project ID for the Sanity client has been updated to 'p5q8f9ac' to align with the new project configuration.

--- sanity/client.ts
+++ sanity/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from 'next-sanity';
 
 export const client = createClient({
-    projectId: 'mmjjwvxw',
+    projectId: 'p5q8f9ac',
     dataset: 'production',
     apiVersion: '2024-01-01',
     useCdn: false,
