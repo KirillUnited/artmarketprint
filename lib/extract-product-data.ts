0494e3bd788d3ad67c453765ba7164d711a02082
dubov <Kirill.Dubov@enterra-inc.com>
2025-04-08 09:16:27 +0300

    feat(extract-product-data): add _id field to product data
    The _id field is added to the extracted product data to align with the backend data structure, ensuring consistency and facilitating future integrations.

--- lib/extract-product-data.ts
+++ lib/extract-product-data.ts
@@ -3,6 +3,7 @@ import { getPrice } from '@/lib/getPrice';
 
 // Product helper functions
 export const extractProductData = (product: Product): ProductData => ({
+    _id: product.id[0]._,
     id: product.id[0]._,
     name: product.product[0]._,
     price: parseFloat(getPrice(product.price[0], 1.1)),
