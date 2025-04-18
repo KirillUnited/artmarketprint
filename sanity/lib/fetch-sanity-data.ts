35dc015fcfbfeacbcc15c8df0510d0f2052b7415
dubov <Kirill.Dubov@enterra-inc.com>
2025-04-18 16:05:59 +0300

    refactor(fetch-sanity-data): disable caching by setting revalidate to 0
    The revalidate option was changed from 30 to 0 to ensure data is fetched on every request, disabling caching. This modification ensures that the latest data is always retrieved, which is critical for scenarios where data freshness is a priority.

--- sanity/lib/fetch-sanity-data.ts
+++ sanity/lib/fetch-sanity-data.ts
@@ -3,8 +3,9 @@ import type { SanityDocument } from 'next-sanity';
 import { client } from '@/sanity/client';
 import { SERVICES_QUERY } from '@/sanity/lib/queries';
 
-const options = { next: { revalidate: 30 } };
-
+// Options object for Next.js data fetching
+// revalidate: 0 means the data will be fetched on every request (no caching)
+const options = { next: { revalidate: 0 } };
 export async function getSanityDocuments(QUERY = SERVICES_QUERY, params={}) {
   try {
     return await client.fetch<SanityDocument[]>(QUERY, params, options);
