1d95d7917fcadc66466e4ac89675377b5ff5ec8f
dubov <Kirill.Dubov@enterra-inc.com>
2025-04-22 19:46:12 +0300

    refactor(services): improve layout and structure of service details page
    Restructure the service details page to enhance readability and maintainability. Replace grid layout with a more flexible article structure, adjust spacing, and improve the organization of components. Additionally, update the FAQ section and contact form layout for better user experience.

--- components/shared/service/Services.tsx
+++ components/shared/service/Services.tsx
@@ -66,47 +66,44 @@ export const Services = async (props: {
 };
 
 export const ServiceDetails = ({ name, description, image, price, advantages, layoutRequirements, children }: ServiceDetailsProps) => (
-	<div className="grid md:grid-cols-2 items-start gap-12">
-		{image && <Image alt={name} className="md:sticky top-16 object-cover w-full aspect-square" height={640} src={`${image}`} width={640} />}
-		<div className="flex flex-col gap-8 md:gap-16">
-			<div className="flex flex-col gap-4 md:gap-6">
-				<div className="flex flex-col gap-2">
-					<span className="text-gray-600">О услуге</span>
-					<h2 className="text-3xl md:text-4xl font-bold break-words">{name}</h2>
+	<article className="flex flex-col gap-8 md:gap-16">
+		<div className="flex flex-col gap-4 md:gap-6">
+			<div className="flex flex-col gap-2">
+				<span className="text-gray-600">О услуге</span>
+				<h2 className="text-3xl md:text-4xl font-bold break-words">{name}</h2>
+			</div>
+			{description && <p className="text-foreground/70 text-balance leading-normal font-light">{description}</p>}
+			{advantages && advantages.length > 0 && (
+				<div className="flex flex-col gap-4">
+					<h3 className="text-xl md:text-2xl font-bold text-gray-900">Преимущества</h3>
+					<ul className="space-y-2 text-gray-600">
+						{advantages?.map((advantage) => (
+							<li key={advantage}>
+								<span className="text-primary font-bold">✔</span> {advantage}
+							</li>
+						))}
+					</ul>
 				</div>
-				{description && <p className="text-foreground/70 text-balance leading-normal font-light">{description}</p>}
-				{advantages && advantages.length > 0 && (
-					<div className="flex flex-col gap-4">
-						<h3 className="text-xl md:text-2xl font-bold text-gray-900">Преимущества</h3>
-						<ul className="space-y-2 text-gray-600">
-							{advantages?.map((advantage) => (
-								<li key={advantage}>
-									<span className="text-primary font-bold">✔</span> {advantage}
-								</li>
-							))}
-						</ul>
-					</div>
-				)}
-				<article className="prose text-balance">{children}</article>
-				{price && (
-					<div className="flex flex-col gap-4">
-						<h3 className="text-xl md:text-2xl font-bold text-gray-900">Цены</h3>
-						<p className="font-semibold text-secondary text-4xl">{price}</p>
-					</div>
-				)}
-				{layoutRequirements && (
-					<div className="flex flex-col gap-4">
-						<h3 className="text-xl md:text-2xl font-bold text-gray-900">Дополнительная информация:</h3>
-						<Alert className="prose flex-col" color="default" icon="info">{layoutRequirements}</Alert></div>
-				)}
-			</div>
-			<div className="flex flex-wrap gap-2 md:gap-4">
-				<BrandModalOffer id={name}/>
-
-				<Button as={Link} className="bg-brand-gradient text-fill-transparent font-semibold flex-1 min-w-40" color="secondary" href={'#contacts'} radius="sm" size="md" variant="ghost">
-					КОНСУЛЬТАЦИЯ
-				</Button>
-			</div>
+			)}
+			<div className="prose max-w-3xl">{children}</div>
+			{layoutRequirements && (
+				<div className="flex flex-col gap-4">
+					<h3 className="text-xl md:text-2xl font-bold text-gray-900">Дополнительная информация:</h3>
+					<Alert className="prose flex-col max-w-full" color="default" icon="info">{layoutRequirements}</Alert></div>
+			)}
+			{price && (
+				<div className="flex flex-col gap-4">
+					<h3 className="text-xl md:text-2xl font-bold text-gray-900">Цены</h3>
+					<p className="font-semibold text-secondary text-4xl">{price}</p>
+				</div>
+			)}
 		</div>
-	</div>
+		<footer className="flex flex-wrap gap-2 md:gap-4">
+			<BrandModalOffer id={name} />
+
+			<Button as={Link} className="bg-brand-gradient text-fill-transparent font-semibold flex-1 min-w-40" color="secondary" href={'#contacts'} radius="sm" size="md" variant="ghost">
+				КОНСУЛЬТАЦИЯ
+			</Button>
+		</footer>
+	</article>
 );
