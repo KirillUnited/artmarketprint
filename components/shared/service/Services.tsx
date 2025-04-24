55da07eb9ecb4815c33c3007f1d93296ba844d34
dubov <Kirill.Dubov@enterra-inc.com>
2025-04-24 17:09:12 +0300

    refactor(service): move ServiceDetails to a separate file
    Extract ServiceDetails component from Services.tsx into a dedicated file for better maintainability and separation of concerns. This change improves code organization and readability by isolating the component logic.

--- components/shared/service/Services.tsx
+++ components/shared/service/Services.tsx
@@ -1,12 +1,5 @@
-import Link from 'next/link';
-import { Button } from '@heroui/button';
-import Image from 'next/image';
-
-import BrandModalOffer from '../../ui/BrandModalOffer';
-
 import ServiceListItems from './_ServiceListItems';
 
-import { ServiceDetailsProps } from '@/types';
 import Section, {
 	SectionButton,
 	SectionDescription,
@@ -15,8 +8,6 @@ import Section, {
 	SectionTitle
 } from '@/components/layout/Section';
 import { SanityDocument } from 'next-sanity';
-import { Alert } from '@heroui/alert';
-import { PriceTable } from '@/components/ui/table';
 
 /**
  * A function that renders a section with a list of services and a button to view all services.
@@ -66,50 +57,4 @@ export const Services = async (props: {
 	);
 };
 
-export const ServiceDetails = ({ name, description, image, price, advantages, layoutRequirements, priceTable, children }: ServiceDetailsProps) => (
-	<article className="flex flex-col gap-8 md:gap-16">
-		<div className="flex flex-col gap-4 md:gap-6">
-			<div className="flex flex-col gap-2">
-				<span className="text-gray-600">О услуге</span>
-				<h2 className="text-3xl md:text-4xl font-bold break-words">{name}</h2>
-			</div>
-			{description && <p className="text-foreground/70 text-balance leading-normal font-light">{description}</p>}
-			{advantages && advantages.length > 0 && (
-				<div className="flex flex-col gap-4">
-					<h3 className="text-xl md:text-2xl font-bold text-gray-900">Преимущества</h3>
-					<ul className="space-y-2 text-gray-600">
-						{advantages?.map((advantage) => (
-							<li key={advantage}>
-								<span className="text-primary font-bold">✔</span> {advantage}
-							</li>
-						))}
-					</ul>
-				</div>
-			)}
-			<div className="prose max-w-3xl">{children}</div>
-			{layoutRequirements && (
-				<div className="flex flex-col gap-4">
-					<h3 className="text-xl md:text-2xl font-bold text-gray-900">Дополнительная информация:</h3>
-					<Alert className="prose flex-col max-w-full" color="default" icon="info">{layoutRequirements}</Alert></div>
-			)}
-			{price && (
-				<div className="flex flex-col gap-4">
-					<h3 className="text-xl md:text-2xl font-bold text-gray-900">Цены</h3>
-					<p className="font-semibold text-secondary text-4xl">{price}</p>
-				</div>
-			)}
-			{
-				priceTable && (
-					<PriceTable items={priceTable} />
-				)
-			}
-		</div>
-		<footer className="flex flex-wrap gap-2 md:gap-4">
-			<BrandModalOffer id={name} />
 
-			<Button as={Link} className="bg-brand-gradient text-fill-transparent font-semibold flex-1 min-w-40" color="secondary" href={'#contacts'} radius="sm" size="md" variant="ghost">
-				КОНСУЛЬТАЦИЯ
-			</Button>
-		</footer>
-	</article>
-);
