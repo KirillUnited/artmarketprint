a4ccc9204e3e78b38f3e24175f11174510b455c2
dubov <Kirill.Dubov@enterra-inc.com>
2025-05-03 18:07:39 +0300

    refactor(products): update product components for consistency and data fetching
    - Remove unused `PackageIcon` import and adjust styling in `ProductsNotFound` for better alignment and spacing
    - Replace `getProductsByLimit` with `getSanityDocuments` in `FeaturedProducts` to fetch data using a Sanity query
    - Add type assertion for `data` in `ProductList` to handle type safety

--- components/shared/product/FeaturedProducts.tsx
+++ components/shared/product/FeaturedProducts.tsx
@@ -1,7 +1,9 @@
 import ProductList from './ProductList';
 
 import Section, { SectionButton, SectionDescription, SectionHeading, SectionSubtitle, SectionTitle } from '@/components/layout/Section';
-import { getProductsByLimit } from '@/lib/actions/product.actions';
+import { getSanityDocuments } from '@/sanity/lib/fetch-sanity-data';
+
+const FEATURED_PRODUCTS_QUERY = `*[_type == 'product'] | order(_createdAt desc) [0...4]`;
 
 export const ProductSectionHeading = ({ title, subtitle, description }: { title?: string; subtitle?: string; description?: string }) => (
     <div className="flex flex-wrap items-end justify-between gap-4">
@@ -14,7 +16,7 @@ export const ProductSectionHeading = ({ title, subtitle, description }: { title?
 );
 
 export const FeaturedProducts = async () => {
-    const data = await getProductsByLimit(4);
+    const data = await getSanityDocuments(FEATURED_PRODUCTS_QUERY);
 
     if (!data || data.length === 0) {
         console.warn('Нет данных о продуктах');
@@ -30,7 +32,7 @@ export const FeaturedProducts = async () => {
                 title={'Популярные товары'}
             />
 
-            <ProductList items={data} />
+            <ProductList items={data as any} />
 
             <SectionButton className="self-start" href={'/products'} label="Все товары"/>
         </Section>
--- components/shared/product/ProductsNotFound.tsx
+++ components/shared/product/ProductsNotFound.tsx
@@ -1,15 +1,14 @@
-import { PackageIcon, PackageSearchIcon } from 'lucide-react'
+import { PackageSearchIcon } from 'lucide-react'
 import React from 'react'
 
 export const ProductsNotFound = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-[400px] h-full bg-gray-50 rounded-lg">
+        <div className="flex flex-col items-center justify-center text-center min-h-[400px] bg-gray-50 rounded-small p-4">
             <PackageSearchIcon className="w-16 h-16 text-gray-300 mb-4" />
             <div className="text-3xl font-semibold text-gray-400 mb-4">
                 Товары не найдены
             </div>
             <p className="text-gray-500 flex items-center gap-2">
-                <PackageIcon className="w-5 h-5" />
                 Попробуйте изменить параметры поиска или фильтрации
             </p>
         </div>
