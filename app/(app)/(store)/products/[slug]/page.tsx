12a6a1e08f862d59c217b3b54dd6096af03e6553
dubov <Kirill.Dubov@enterra-inc.com>
2025-05-08 17:35:06 +0300

    feat(product): add ProductTabs component for product description and options
    This commit introduces the `ProductTabs` component, which organizes product descriptions and options into a tabbed interface. The component is integrated into the product page, replacing the previous inline display of product details. Additionally, the `@heroui/tabs` dependency is added to support the new component.

--- app/(app)/(store)/products/[slug]/page.tsx
+++ app/(app)/(store)/products/[slug]/page.tsx
@@ -11,6 +11,7 @@ import AddToBasketButton from '@/components/ui/AddToBasketButton';
 import ProductPrice from '@/components/shared/product/ProductPrice';
 import { getSanityDocuments } from '@/sanity/lib/fetch-sanity-data';
 import { Chip } from '@heroui/chip';
+import { ProductTabs } from '@/components/shared/product/ProductTabs';
 
 export interface Props {
     slug: string,
@@ -108,11 +109,11 @@ export default async function ProductPage({ params }: { params: Promise<Props> }
                                 </p>
                             </CardBody>
                             <CardBody>
-                                <ProductDetails 
-                                    items={items} 
+                                <ProductDetails
+                                    items={items}
                                     sizes={sizes}
                                     colors={colors}
-                                    color={items?.[0]?.color || ''} 
+                                    color={items?.[0]?.color || ''}
                                     size={sizes?.[0] || ''}
                                 />
                             </CardBody>
@@ -120,16 +121,11 @@ export default async function ProductPage({ params }: { params: Promise<Props> }
                                 <AddToBasketButton product={product as any} />
                             </CardFooter>
                         </Card>
-                        <p className='text-foreground font-semibold text-sm'>Характеристики</p>
-                        <article className="prose" dangerouslySetInnerHTML={{ __html: variation_description }} />
                     </div>
                 </div>
-                <article className="prose mt-8 max-w-full">
-                    <h2>Описание</h2>
-                    <p>
-                        {general_description}
-                    </p>
-                </article>
+                
+                <ProductTabs description={general_description} options={variation_description}/>
+
             </Section>
             <RelatedProducts product={product} />
         </>
