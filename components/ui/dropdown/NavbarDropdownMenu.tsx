59c07e1a2c92d0a083335ca2547a8a7e1652746f
dubov <Kirill.Dubov@enterra-inc.com>
2025-05-20 18:55:55 +0300

    feat(navbar): add image support to dropdown menu items
    Enhance the NavbarDropdownMenu component to display images alongside service items. This includes adding the `image` field to the query and integrating the `urlFor` utility from Sanity to handle image URLs. The layout and styling of the dropdown menu have also been adjusted to accommodate the new image content.

--- components/ui/dropdown/NavbarDropdownMenu.tsx
+++ components/ui/dropdown/NavbarDropdownMenu.tsx
@@ -1,21 +1,24 @@
+import { urlFor } from "@/sanity/lib/image";
 import { Button } from "@heroui/button";
 import { Dropdown, DropdownItem, DropdownMenu, DropdownTrigger } from "@heroui/dropdown";
 import { NavbarItem } from "@heroui/navbar";
 import { ChevronDownIcon, SettingsIcon, TagsIcon } from "lucide-react";
+import Image from "next/image";
 
 type HeaderDropdownMenuProps = {
-	triggerLabel: string;
-	items: {
-		title: string; url?: string; description?: string;
-		services?: any;
-	}[];
+    triggerLabel: string;
+    items: {
+        title: string; url?: string; description?: string;
+        services?: any;
+    }[];
 };
 
 export const NavbarDropdownMenu = ({ triggerLabel, items }: HeaderDropdownMenuProps) => {
+    console.log(items);
     return (
         <Dropdown
             classNames={{
-                content: 'rounded-md',
+                content: 'rounded-small',
             }}
         >
             <NavbarItem>
@@ -34,12 +37,26 @@ export const NavbarDropdownMenu = ({ triggerLabel, items }: HeaderDropdownMenuPr
             </NavbarItem>
             <DropdownMenu
                 aria-label="Услуги"
-                className="w-[320px]"
+                classNames={{
+                    base: 'max-w-[90vw] rounded-small',
+                    list: 'grid grid-cols-4 gap-4',
+                }}
                 itemClasses={{
-                    base: 'gap-4 rounded-md data-[hover=true]:bg-brand-gradient data-[hover=true]:text-fill-transparent',
-                    title: 'font-semibold truncate max-w-full',
+                    base: 'gap-4 rounded-md data-[hover=true]:bg-brand-gradient data-[hover=true]:text-fill-transparent items-start',
+                    title: 'font-semibold whitespace-normal max-w-full',
+                    wrapper: 'max-w-[calc(100%-64px)]',
                 }}
             >
+                {items[0]?.services?.map((item: any) => (
+                    <DropdownItem
+                        key={item.title}
+                        description={item.description}
+                        href={`${items[0].url}/${item.url}`}
+                        startContent={<Image src={urlFor(item.image?.asset).url()} className="object-contain aspect-square" alt={item.title} width={64} height={64} quality={10} />}
+                    >
+                        {item.title}
+                    </DropdownItem>
+                ))}
                 <DropdownItem
                     key={items[0].title}
                     classNames={{
@@ -47,15 +64,6 @@ export const NavbarDropdownMenu = ({ triggerLabel, items }: HeaderDropdownMenuPr
                     }} href={items[0].url} startContent={triggerLabel === 'Услуги' ? <SettingsIcon /> : <TagsIcon />}>
                     {items[0].title}
                 </DropdownItem>
-                {items[0]?.services?.map((item: any) => (
-                    <DropdownItem
-                        key={item.title}
-                        description={item.description}
-                        href={`${items[0].url}/${item.url}`}
-                    >
-                        {item.title}
-                    </DropdownItem>
-                ))}
             </DropdownMenu>
         </Dropdown>
     );
