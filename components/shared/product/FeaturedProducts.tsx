8831b9ee170c15bc03b8bcd73707e4ecadb5e08d
dubov <Kirill.Dubov@enterra-inc.com>
2025-05-21 23:47:26 +0300

    refactor(FeaturedProducts): change product order from descending to ascending
    The query for fetching featured products was modified to order products by creation date in ascending order instead of descending. This change aligns with the requirement to display the oldest products first.

--- components/shared/product/FeaturedProducts.tsx
+++ components/shared/product/FeaturedProducts.tsx
@@ -3,7 +3,7 @@ import ProductList from './ProductList';
 import Section, { SectionButton, SectionDescription, SectionHeading, SectionSubtitle, SectionTitle } from '@/components/layout/Section';
 import { getSanityDocuments } from '@/sanity/lib/fetch-sanity-data';
 
-const FEATURED_PRODUCTS_QUERY = `*[_type == 'product'] | order(_createdAt desc) [0...4]`;
+const FEATURED_PRODUCTS_QUERY = `*[_type == 'product'] | order(_createdAt asc) [0...4]`;
 
 export const ProductSectionHeading = ({ title, subtitle, description }: { title?: string; subtitle?: string; description?: string }) => (
     <div className="flex flex-wrap items-end justify-between gap-4">
