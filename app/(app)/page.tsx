09758a0e9e66dcc6f895f16e429831059d17e3ec
dubov <Kirill.Dubov@enterra-inc.com>
2025-06-30 11:34:32 +0300

    refactor(page): remove unused product data imports and logging
    Clean up home page by removing unused product data fetching logic and debug console log

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -2,8 +2,6 @@ import { HOME_PAGE_QUERY } from '@/sanity/lib/queries/page.query';
 import { PageBuilder } from '@/components/PageBuilder';
 import { sanityFetch } from '@/sanity/lib/sanityFetch';
 import { Metadata } from 'next';
-import {getXmlDataJSON} from "@/lib/products/data";
-import {Companies} from "@/lib/products/companies";
 
 export async function generateMetadata(): Promise<Metadata> {
 	const data: any = await sanityFetch({ query: HOME_PAGE_QUERY });
@@ -15,10 +13,7 @@ export async function generateMetadata(): Promise<Metadata> {
 }
 
 export default async function Home() {
-	const _data = await getXmlDataJSON(Companies.MARKLI.product_data_url);
 	const data: any = await sanityFetch({ query: HOME_PAGE_QUERY });
-
-	console.log(_data?.xml_catalog?.shop[0]);
 
 	return (
 		<>
