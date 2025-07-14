a8ada7f788dc2a494e71536689ea082eabedd072
dubov <Kirill.Dubov@enterra-inc.com>
2025-07-14 14:23:24 +0300

    fix(dropdown): handle null image asset in NavbarDropdownMenu
    feat(category): add fallback image and logging in FeaturedCategories
    - Add console.log for debugging FeaturedCategoryThumb
    - Implement fallback image when no image is available

--- components/shared/category/FeaturedCategories.tsx
+++ components/shared/category/FeaturedCategories.tsx
@@ -68,6 +68,7 @@ interface FeaturedCategoryThumbProps {
 }
 
 export const FeaturedCategoryThumb = ({item}: FeaturedCategoryThumbProps) => {
+	console.log(item);
 	return (
 		<Card isFooterBlurred as={Link} className="h-full group relative" href={`/categories/${item?.slug?.current || item.currentSlug}`} radius="sm">
 			<Image
@@ -77,9 +78,10 @@ export const FeaturedCategoryThumb = ({item}: FeaturedCategoryThumbProps) => {
 				alt={item.title}
 				className="z-0 w-full h-full object-cover aspect-square"
 				radius="sm"
-				src={item.imageUrl ? item.imageUrl : getUrlFor(item.image)}
+				src={item.imageUrl ? item.imageUrl : item.image ? getUrlFor(item.image) : '/images/product-no-image.jpg'}
 				width={220}
 				height={220}
+				fallbackSrc="/images/product-no-image.jpg"
 			/>
 			<CardFooter className={clsx('absolute bg-black/40 bottom-0 w-full z-10 p-0')}>
 				<div className="flex flex-col gap-2 p-3 w-full">
