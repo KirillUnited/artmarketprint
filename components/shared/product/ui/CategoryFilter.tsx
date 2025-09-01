f44ce4e9e334fa4646c87ac16706a3533d48c755
dubov <Kirill.Dubov@enterra-inc.com>
2025-09-01 21:32:49 +0300

    feat(product): enhance category filter with images and grid layout
    Add image support and improve styling for category filter items. Implement responsive grid layout with smooth scrolling behavior for better mobile experience.

--- components/shared/product/ui/CategoryFilter.tsx
+++ components/shared/product/ui/CategoryFilter.tsx
@@ -1,15 +1,26 @@
 // components/CategoryFilter.tsx
 import Link from 'next/link';
+import {clsx} from "clsx";
+import styles from './styles.module.css';
+import {urlFor} from "@/sanity/lib/image";
+import Image from "next/image";
 
 export default function CategoryFilter({categories, active}: {categories: any[]; active: string}) {
 	return (
-		<div className="flex gap-4 overflow-x-auto">
-			<Link href="/products/categories/all" className={active === 'all' ? 'font-bold' : ''}>
-				ВСЕ
-			</Link>
+		<div className={clsx(styles.CategoryFilter)}>
+			{/*<Link href="/products/categories/all" className={active === 'all' ? 'font-bold' : ''}>*/}
+			{/*	ВСЕ*/}
+			{/*</Link>*/}
 			{categories.map((cat) => (
-				<Link key={cat._id} href={`/products/categories/${cat.currentSlug}`} className={active === cat.currentSlug ? 'font-bold' : ''}>
-					{cat.title}
+				<Link
+					key={cat._id}
+					href={`/products/categories/${cat.currentSlug}`}
+					className={clsx(active === cat.currentSlug ? 'font-bold' : '',
+						'hover:bg-brand-gradient ',
+						styles.CategoryFilterItem
+					)}>
+					<Image alt={cat.title} className="object-cover aspect-square rounded-small w-10 h-10" width={40} height={40} quality={10} src={urlFor(cat.image?.asset).width(64).height(64).url()} />
+					<span className='flex-1 text-xs py-1'>{cat.title}</span>
 				</Link>
 			))}
 		</div>
