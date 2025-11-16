f6f064511c7b44bd8a9a2f6505bb32770d4cf390
dubov <Kirill.Dubov@enterra-inc.com>
2025-11-16 19:31:51 +0300

    feat(blog): add breadcrumb navigation to blog page
    Fetch breadcrumb links from Sanity and display them above the blog posts section to improve navigation.

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -4,6 +4,9 @@ import {Metadata} from 'next';
 import {getAllPosts} from '@/components/blog/lib/fetch-data';
 import {PostListing} from '@/components/blog';
 import NotFound from '@/app/blog/not-found';
+import BaseBreadcrumb from '@/components/ui/Breadcrumb';
+import {getSanityDocuments} from '@/sanity/lib/fetch-sanity-data';
+import {NAVIGATION_QUERY} from '@/sanity/lib/queries';
 
 export const metadata: Metadata = {
 	title: 'Наш Блог',
@@ -12,6 +15,7 @@ export const metadata: Metadata = {
 
 export default async function BlogPage() {
 	const posts = await getAllPosts();
+	const breadcrumbs = (await getSanityDocuments(NAVIGATION_QUERY))[0].links;
 
 	console.log('Blog Page posts', posts);
 
@@ -20,6 +24,14 @@ export default async function BlogPage() {
 	return (
 		<main className="container mx-auto py-8">
 			<h1 className="text-3xl font-bold mb-8">Наш Блог</h1>
+			{/* Breadcrumb navigation */}
+			<section>
+				<div className="container">
+					<div className="mt-10 mb-6">
+						<BaseBreadcrumb items={breadcrumbs} section={'Блог'} />
+					</div>
+				</div>
+			</section>
 			<PostListing posts={posts} />
 		</main>
 	);
