dc2f936fa79423b56f11f565e7a4742b1fad8ef1
dubov <Kirill.Dubov@enterra-inc.com>
2025-11-19 19:55:40 +0300

    refactor(blog): improve code formatting and imports organization
    - Reorder imports and fix formatting in PostMetadata component
    - Clean up PostAvatar component with better type annotations and formatting

--- components/blog/ui/PostAvatar.tsx
+++ components/blog/ui/PostAvatar.tsx
@@ -1,25 +1,30 @@
-import React from 'react'
-import { Post } from '../lib/types';
-import { urlFor } from '@/sanity/lib/image';
+import React, {JSX} from 'react';
 import Image from 'next/image';
+
+import {Post} from '../lib/types';
+
+import {urlFor} from '@/sanity/lib/image';
 
 /**
  * A function that renders a post avatar component.
- * @param {{post: {author: {name: string}}}} props - The props object with required properties.
+ * @param {post: Post} props - The props object with required properties.
  * @returns {React.ReactElement} A JSX element that represents the post avatar.
  */
-export default function PostAvatar({ post}: {post: Post}) {
-    return (
-        <div className={'flex items-center gap-2 text-xs'}>
-            <Image
-                priority
-                alt={post.author?.name || ''}
-                className="w-8 h-8 object-cover rounded-full"
-                height={64}
-                src={urlFor(post?.author?.image || '').width(64).format('webp').url()}
-                width={64}
-            />
-            <span>{post.author?.name || ''}</span>
-        </div>
-    )
+export default function PostAvatar({post}: {post: Post}): JSX.Element {
+	return (
+		<div className={'flex items-center gap-2 text-xs'}>
+			<Image
+				priority
+				alt={post.author?.name || ''}
+				className="w-8 h-8 object-cover rounded-full"
+				height={64}
+				src={urlFor(post?.author?.image || '')
+					.width(64)
+					.format('webp')
+					.url()}
+				width={64}
+			/>
+			<span>{post.author?.name || ''}</span>
+		</div>
+	);
 }
