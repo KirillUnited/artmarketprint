3e9982d3c4134ad1a3d4daa0d75ccc0797855452
dubov <Kirill.Dubov@enterra-inc.com>
2025-12-02 16:57:14 +0300

    feat(blog): enhance BlogCategoryPage with category metadata and improved layout

--- app/blog/categories/[category]/page.tsx
+++ app/blog/categories/[category]/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {Metadata} from 'next';
 
 import {sanityFetch} from '@/sanity/lib/sanityFetch';
-import {POSTS_BY_CATEGORY_QUERY} from '@/components/blog/lib/queries';
+import {CATEGORY_BY_SLUG_QUERY, POSTS_BY_CATEGORY_QUERY} from '@/components/blog/lib/queries';
 import NotFound from '@/app/blog/not-found';
 import Section, {SectionHeading} from '@/components/layout/Section';
 import {PostListing} from '@/components/blog';
@@ -19,13 +19,17 @@ export const metadata: Metadata = {
 export default async function BlogCategoryPage({params}: {params: Promise<Props>}) {
 	const {category} = await params;
 	const posts: any = await sanityFetch({query: POSTS_BY_CATEGORY_QUERY, params: {categorySlug: category}});
+	const categoryData: any = await sanityFetch({query: CATEGORY_BY_SLUG_QUERY, params: {slug: category}});
 
 	if (!Array.isArray(posts) || posts.length === 0) return <NotFound />;
 
 	return (
 		<Section>
 			<SectionHeading>
-				<h1 className="text-3xl font-bold">Наш Блог</h1>
+				<h1 className="text-3xl font-semibold">Наш Блог</h1>
+				<h2 className="text-xl">
+					Категория: <span className="font-semibold">{categoryData?.title}</span>
+				</h2>
 			</SectionHeading>
 			<PostListing posts={posts} />
 		</Section>
