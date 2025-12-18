90a0dc2dc2385fa6ccdb41d9135629c85fd7cf8e
dubov <Kirill.Dubov@enterra-inc.com>
2025-12-18 15:02:55 +0300

    refactor(image): use env vars for Sanity project config
    Replace hardcoded project ID and dataset with environment variables to improve security and flexibility

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,7 +1,7 @@
 import {createImageUrlBuilder, SanityImageSource} from '@sanity/image-url'
 
 // https://www.sanity.io/docs/image-url
-const builder = createImageUrlBuilder({ projectId: 'p5q8f9ac', dataset: 'production' })
+const builder = createImageUrlBuilder({ projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!, dataset: process.env.NEXT_PUBLIC_SANITY_DATASET! })
 
 export const urlFor = (source: SanityImageSource) => {
     return builder.image(source).format('webp').fit('crop')
