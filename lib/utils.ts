7d948f885416fd770e08f6ebd1b5c2fe6c444c43
dubov <Kirill.Dubov@enterra-inc.com>
2026-02-11 16:24:40 +0300

    refactor(utils): remove unused JSON file handling functions
    - Deleted commented-out code related to JSON file saving and reading, streamlining the utils file for better readability and maintenance.

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,8 +2,6 @@ import { client } from '@/sanity/client';
 
 import { createImageUrlBuilder, type SanityImageSource } from '@sanity/image-url';
 
-// const fs = require('fs').promises;
-
 import { clsx, type ClassValue } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 
@@ -18,34 +16,3 @@ export const getUrlFor = (source: SanityImageSource) => {
 
   return builder.image(source).url();
 }
-
-// export async function saveJsonToFile(filename: string, data: any) {
-//   try {
-//     const filePath = path.resolve(process.cwd(), filename);
-//     const jsonData = JSON.stringify(data, null, 2); // Форматирование с отступами
-//
-//     await fs.writeFile(filePath, jsonData, 'utf8');
-//     console.log(`✅ Файл успешно сохранен: ${filePath}`);
-//   } catch (error) {
-//     console.error('❌ Ошибка записи JSON-файла:', error);
-//   }
-// }
-//
-// const DATA_FILE_NAME = 'products.json';
-// const DATA_FILE_PATH = path.join(process.cwd(), '_data', DATA_FILE_NAME);
-//
-// // Кешируем данные, чтобы не загружать файл при каждом запросе
-// let cachedData: any | null = null;
-//
-// export async function getJsonFileData(): Promise<any> {
-//   if (cachedData) return cachedData;
-//
-//   try {
-//     const fileContent = await fs.readFile(DATA_FILE_PATH, 'utf-8');
-//     cachedData = JSON.parse(fileContent);
-//     return cachedData;
-//   } catch (error) {
-//     console.error('Error reading JSON file:', error);
-//     throw new Error(`Failed to load '${DATA_FILE_NAME}' file data`);
-//   }
-// }
