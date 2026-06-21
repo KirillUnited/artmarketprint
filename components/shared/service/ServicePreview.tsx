7403d921ff4d446d59cacbc30afe4c9167cc9a91
dubov <Kirill.Dubov@enterra-inc.com>
2026-06-21 10:26:12 +0300

    style(ServicePreview): adjust layout and add currency symbols

--- components/shared/service/ServicePreview.tsx
+++ components/shared/service/ServicePreview.tsx
@@ -10,10 +10,11 @@ import {SanityDocument} from 'next-sanity';
 
 import {urlFor} from '@/sanity/lib/image';
 import {ProjectTagList} from '@/components/shared/project';
+import {CURRENCIES_SYMBOLS} from '@/lib/products/companies';
 
 export function ServicePreview({service}: {service: SanityDocument}): JSX.Element {
 	return (
-		<Card isFooterBlurred as={Link} className="h-full group relative mx-2 w-full" href={`/services/${service.currentSlug || service.slug?.current}`} radius="sm" shadow="none">
+		<Card isFooterBlurred as={Link} className="group relative mx-2 h-full w-full" href={`/services/${service.currentSlug || service.slug?.current}`} radius="sm" shadow="none">
 			<div className="absolute top-3 left-3 z-10 flex flex-wrap gap-2">
 				{service?.service_tags?.length > 0 && <ProjectTagList color="primary" tags={service.service_tags} />}
 				{service?.category_tags?.length > 0 && <ProjectTagList color="secondary" tags={service.category_tags} />}
@@ -23,7 +24,7 @@ export function ServicePreview({service}: {service: SanityDocument}): JSX.Elemen
 					removeWrapper
 					alt={service.title}
 					as={NextImage}
-					className="z-0 w-full h-full object-cover aspect-square"
+					className="z-0 aspect-square h-full w-full object-cover"
 					fallbackSrc="/images/product-no-image.jpg"
 					height={0}
 					quality={50}
@@ -33,16 +34,18 @@ export function ServicePreview({service}: {service: SanityDocument}): JSX.Elemen
 					width={0}
 				/>
 			) : (
-				<Image removeWrapper alt="No image" as={NextImage} className="z-0 w-full h-full object-cover aspect-square" height={220} radius="sm" src="/images/product-no-image.jpg" width={220} />
+				<Image removeWrapper alt="No image" as={NextImage} className="z-0 aspect-square h-full w-full object-cover" height={220} radius="sm" src="/images/product-no-image.jpg" width={220} />
 			)}
-			<CardFooter className={clsx('absolute bg-white/75 bottom-0 w-full z-10 p-0 backdrop-blur-lg')}>
-				<div className="flex flex-col gap-2 p-3 w-full">
+			<CardFooter className={clsx('absolute bottom-0 z-10 w-full bg-white/75 p-0 backdrop-blur-lg')}>
+				<div className="flex w-full flex-col gap-2 p-3">
 					<div className="flex flex-col gap-2">
-						<p className="flex flex-col gap-2 font-semibold line-clamp-2 leading-tight" title={service.title}>
+						<p className="line-clamp-2 flex flex-col gap-2 leading-tight font-semibold" title={service.title}>
 							{service.title}
-							<span className="text-primary text-xl font-bold line-clamp-1">{service.price}</span>
+							<span className="text-primary line-clamp-1 text-xl font-bold">
+								{service.price} {CURRENCIES_SYMBOLS['BYN']}
+							</span>
 						</p>
-						<p className={clsx('text-xs line-clamp-2', 'grid grid-rows-[0fr] group-hover:grid-rows-[1fr] transition-all duration-500 overflow-hidden')} title={service.description}>
+						<p className={clsx('line-clamp-2 text-xs', 'grid grid-rows-[0fr] overflow-hidden transition-all duration-500 group-hover:grid-rows-[1fr]')} title={service.description}>
 							<span className="line-clamp-4">{service.description}</span>
 						</p>
 					</div>
